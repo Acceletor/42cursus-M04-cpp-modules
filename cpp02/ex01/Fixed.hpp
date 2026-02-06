93ecb6ee37608619182ad9ed2cb94e2340d3e739
Karintorn Suebthawilkul <karinsueb@gmail.com>
2026-02-06 17:25:47 +0100

    cpp02 ex01

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed {
+    private:
+        int _fixed_point_number;
+        static const int _number_of_fractional_bits;
+    public:
+        Fixed();
+        Fixed(const int number);
+        Fixed(const float number);
+        Fixed(const Fixed& other);
+        Fixed& operator=(const Fixed& other);
+        ~Fixed();
+
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+        float toFloat(void) const;
+        int toInt(void) const;
+};
+
+std::ostream &operator<<(std::ostream &os, Fixed const &fixed);
+
+#endif
