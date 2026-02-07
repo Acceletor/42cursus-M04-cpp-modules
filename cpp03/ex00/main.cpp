12db92bb5ff73ea2db493ac74423a4d29e932e13
Karintorn Suebthawilkul <karinsueb@gmail.com>
2026-02-07 16:19:39 +0100

    cpp3 ex00

---
+++ cpp03/ex00/main.cpp
@@ -0,0 +1,43 @@
+#include " ClapTrap.hpp"
+
+int main() {
+
+    ClapTrap defaultClap;
+    ClapTrap tom("Tom");
+    ClapTrap copy_tom(tom);
+    tom = copy_tom;
+    ClapTrap ben("Ben");
+    
+    std::cout << "--------------Test(Take damage) -----------\n";
+    tom.printStatus();
+    tom.takeDamage(5);
+    tom.printStatus();
+
+    ben.printStatus();
+    ben.takeDamage(10);
+    ben.printStatus();
+    
+    std::cout << "--------------Test(attack)-----------\n";
+    tom.printStatus();
+    tom.attack("a monster");
+    tom.printStatus();
+    
+    ben.printStatus();
+    ben.attack("a sea monster");
+    ben.printStatus();
+
+    std::cout << "--------------Test(repair)-----------\n";
+    tom.printStatus();
+    tom.beRepaired(3);
+    tom.printStatus();
+    
+    ben.printStatus();
+    ben.beRepaired(3);
+    ben.printStatus();
+
+    std::cout << "--------------Copy one-----------\n";
+    copy_tom.printStatus();
+    defaultClap.printStatus();
+    
+    return (0);
+}
